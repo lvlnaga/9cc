fa329ad861a7e5a1e7073c093c47e3aab24642ae
lVlnaga <cid15135@gmail.com>
2022-12-30 15:25:21 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,38 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
+  /* 最初のおまじない */
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
+
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+
+  //最初の数字を取得してその次の文字にポインタがセットされる
+  printf("  mov rax, %ld\n", strtol(p, &p, 10)); 
+  
+  while (*p) //引数がなくなるまで続ける
+  {
+    if (*p == '+') //+があったら、ポインタを次にすすめて、+の次の数字を足してあげる
+    {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10)); 
+      continue;
+    }
+    
+    if (*p == '-') ////-があったら、ポインタを次にすすめて、-の次の数字を引いてあげる
+    {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10)); 
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+  
+  
   printf("  ret\n");
   return 0;
 }
